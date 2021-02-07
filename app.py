0da9df4125453f5bcc61e98f418c33f09ac936bd
theriley106 <christopherlambert106@gmail.com>
2021-02-06 19:49:22 -0800

    Added all files

--- app.py
+++ app.py
@@ -1,31 +1,11 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, redirect
 import json
-import getData
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='/static/')
 
-@app.template_filter()
-def currencyFormat(value):
-    value = float(value)
-    return "${:,.0f}".format(value * 1000000)
-    
 @app.route('/', methods=['GET'])
 def index():
-    ranking = getData.get_from_mongo().get('ranking', [])
-    if len(ranking) < 5:
-        return 'error', 404
-    return render_template("index.html", ranking=ranking, result=ranking[0]["personName"] == "Elon Musk")
-
-@app.route('/no', methods=['GET'])
-def indexno():
-    ranking = getData.get_from_mongo().get('ranking', [])
-    if len(ranking) < 5:
-        return 'error', 404
-    return render_template("index.html", ranking=ranking, result=ranking[0]["personName"] != "Elon Musk")
-
-@app.route('/debug', methods=['GET'])
-def debug():
-    return jsonify(getData.get_from_mongo())
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
--- keys.py
+++
@@ -1,2 +0,0 @@
-API_KEY = "0NM8OR01TPDQF8RZ"
-MONGO_URL = "mongodb+srv://chrisUser:O0OjTzcG67vOHyd3@cluster0.i0fix.mongodb.net/newDatabase?retryWrites=true&w=majority"
